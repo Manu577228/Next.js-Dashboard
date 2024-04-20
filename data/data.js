ae97125732d6f9ed2ba597ef66fdc97e1b8b1205
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2024-04-20 12:00:42 +0530

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'Gowri',
+      last: 'Bharadwaj',
+    },
+    total: 2795.95,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Shalini',
+      last: 'Sha',
+    },
+    total: 1195.95,
+    status: 'Processing',
+    method: 'PayPal',
+    date: '23 Minutes ago',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Bharadwaj',
+      last: 'Swe',
+    },
+    total: 495.85,
+    status: 'Completed',
+    method: 'Visa',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Durga',
+      last: 'devi',
+    },
+    total: 150.45,
+    status: 'Processing',
+    method: 'MasterCard',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Simran',
+      last: 'Singh',
+    },
+    total: 175.25,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'simmi',
+      last: 'Coela',
+    },
+    total: 1295.75,
+    status: 'Completed',
+    method: 'Check',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Manu',
+      last: 'Jhu',
+    },
+    total: 89.95,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'shiva',
+      last: 'aadi',
+    },
+    total: 689.45,
+    status: 'Completed',
+    method: 'Visa',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Neha',
+      last: 'Agarwal',
+    },
+    total: 14.99,
+    status: 'Completed',
+    method: 'PayPal',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'gani',
+      last: 'Rao',
+    },
+    total: 218.99,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '1 Day ago',
+  },
+];
